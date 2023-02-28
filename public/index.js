1317b7bcb8f9ca32de5519ccecca428f6430d55e
Daniel Tabares Pavas <daniel.tabares1@udea.edu.co>
2023-02-27 20:15:59 -0500

    Node project started, express instalation

---
+++ public/index.js
@@ -0,0 +1,11 @@
+const server = require('express');
+const app = server();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
